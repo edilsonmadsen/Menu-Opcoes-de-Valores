b7a672a6dc312efd287f92468bd558a95c0c3dc5
Edilson Madsen <edilson_madsen@hotmail.com>
2021-10-29 09:53:09 -0300

    commit

---
+++ menuopcoesvalores.py
@@ -0,0 +1,36 @@
+from time import sleep #leitura de dois valores e menu de opções
+n1 = int(input('Pr imeiro valor: '))
+n2 = int(input('Segundo valor: '))
+opção = 0
+while opção != 5:
+    print('''    [ 1 ] somar
+    [ 2 ] multiplicar
+    [ 3 ] maior
+    [ 4 ] novos números
+    [ 5 ] sair do programa''')
+    opção = int(input('>>>>> Qual é a sua opção? '))
+    if opção == 1:
+        soma = n1 + n2
+        print('A soma entre {} + {} é {}'.format(n1, n2, soma))
+    elif opção == 2:
+        produto = n1 * n2
+        print('A multiplicação entre {} x {} é {}'.format(n1, n2, produto))
+    elif opção == 3:
+        if n1 > n2:
+            maior = n1
+        elif n1 < n2:
+            maior = n2
+            print('Entre {} e {} o maior valor é {}'.format(n1, n2, maior))
+        else:
+            print('Os números são iguais!')
+    elif opção == 4:
+        print('Informe os números novamente: ')
+        n1 = int(input('Primeiro valor: '))
+        n2 = int(input('Segundo valor: '))
+    elif opção == 5:
+        print('Finalizando...')
+    else:
+        print('Opção inválida. Tente novamente!')
+    print('=-=' * 10)
+    sleep(2)
+print('Fim do programa! Volte sempre!')
